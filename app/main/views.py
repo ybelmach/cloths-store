df43cae3b58df37595f5dfe3a3a964d550e4fd4e
ybelmach <yaroslav.belmach.wk@gmail.com>
2024-03-23 15:33:44 +0300

    After adding fixtures for goods

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
+from goods.models import Categories
+
 
 def index(request):
+
+    categories = Categories.objects.all()
+
     contex = {
         'title': 'Home - Главная',
         'content': 'Магазин мебели HOME',
+        'categories': categories,
     }
     return render(request, 'main/index.html', contex)
 
