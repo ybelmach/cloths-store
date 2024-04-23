36ac31165368def288faefc65f31cded60a0bd98
ybelmach <yaroslav.belmach.wk@gmail.com>
2024-04-23 10:43:13 +0300

    The product filter on the site page and urlencode

--- app/goods/views.py
+++ app/goods/views.py
@@ -7,6 +7,9 @@ from goods.models import Products
 def catalog(request, category_slug):
 
     page = request.GET.get('page', 1)
+    # Фильтры
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
 
     # Реализация сортировки по категории
     if category_slug == 'all':
@@ -15,9 +18,15 @@ def catalog(request, category_slug):
         # Вызов Page not found, если в БД ничего нет
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
+    # Добавление к goods отфильтрованных товаров
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
+
     # Создание пагинатора (по 3 товара на страницу)
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(int(page))  # Переменная для пользователся
+    current_page = paginator.page(int(page))  # Переменная для пользователя
 
     context = {
         'title': 'Home - Каталог',
