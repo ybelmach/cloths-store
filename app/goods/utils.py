8dff3309e3fc2a73af4b08b78dc2c5253362cd19
ybelmach <yaroslav.belmach.wk@gmail.com>
2024-04-25 23:15:12 +0300

    PostgreSQL data base connected & full text search

--- app/goods/utils.py
+++ app/goods/utils.py
@@ -1,4 +1,5 @@
-from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+# from django.db.models import Q
 
 from goods.models import Products
 
@@ -7,13 +8,17 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
 
-    keywords = [word for word in query.split() if len(word) > 2]  # Получение слов без предлогов
+    vector = SearchVector('name', 'description')
+    query = SearchQuery(query)
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
 
-    q_objects = Q()  # Если пользователь введёт како-либо предлог, то ему выдастся весь список,
-    # т.к. эта переменная будет пустая
-
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
-
-    return Products.objects.filter(q_objects)
+    # keywords = [word for word in query.split() if len(word) > 2]  # Получение слов без предлогов
+    #
+    # q_objects = Q()  # Если пользователь введёт како-либо предлог, то ему выдастся весь список,
+    # # т.к. эта переменная будет пустая
+    #
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+    #
+    # return Products.objects.filter(q_objects)
