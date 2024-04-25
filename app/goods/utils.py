97ab4c7cf5b5f53829e8df872c0d22e7083b9bfa
ybelmach <yaroslav.belmach.wk@gmail.com>
2024-04-25 21:53:33 +0300

    Simple search created

---
+++ app/goods/utils.py
@@ -0,0 +1,19 @@
+from django.db.models import Q
+
+from goods.models import Products
+
+
+def q_search(query):
+    if query.isdigit() and len(query) <= 5:
+        return Products.objects.filter(id=int(query))
+
+    keywords = [word for word in query.split() if len(word) > 2]  # Получение слов без предлогов
+
+    q_objects = Q()  # Если пользователь введёт како-либо предлог, то ему выдастся весь список,
+    # т.к. эта переменная будет пустая
+
+    for token in keywords:
+        q_objects |= Q(description__icontains=token)
+        q_objects |= Q(name__icontains=token)
+
+    return Products.objects.filter(q_objects)
--- app/goods/views.py
+++ app/goods/views.py
@@ -2,18 +2,22 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_list_or_404
 
 from goods.models import Products
+from goods.utils import q_search
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
 
     page = request.GET.get('page', 1)
     # Фильтры
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
     # Реализация сортировки по категории
     if category_slug == 'all':
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         # Вызов Page not found, если в БД ничего нет
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
