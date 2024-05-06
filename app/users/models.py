d662dce019bbab2349b10f7767eb9e053dd00b72
ybelmach <yaroslav.belmach.wk@gmail.com>
2024-05-06 21:37:16 +0300

    Login form and controller are ready

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='Аватар')
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'пользователи'
+
+    def __str__(self):
+        return self.username
--- app/users/views.py
+++ app/users/views.py
@@ -1,9 +1,29 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from users.forms import UserLoginForm
+from django.urls import reverse
 
 def login(request):
+
+    # Создание формы, авторизация
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                # Перенаправление на главнуюстраницу
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
     contex = {
-        'title': 'Home - Авторизация'
+        'title': 'Home - Авторизация',
+        'form': form,
     }
     return render(request, 'users/login.html', contex)
 
