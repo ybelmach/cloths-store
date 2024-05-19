e342c120351e220ac27bf60bcf6e5ff5e12e8eee
ybelmach <yaroslav.belmach.wk@gmail.com>
2024-05-19 11:25:36 +0300

    Admin panel setup

--- app/goods/admin.py
+++ app/goods/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from goods.models import Categories, Products
 
+
 # admin.site.register(Categories)
 # admin.site.register(Products)
 
@@ -9,6 +10,12 @@ from goods.models import Categories, Products
 class CategoriesAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
+
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name', 'quantity', 'price', 'discount']
+    list_editable = ['discount']
+    search_fields = ['name', 'description']
+    list_filter = ['discount', 'quantity', 'category']
+    fields = ['name', 'category', 'slug', 'description', 'image', ('price', 'discount'), 'quantity']
